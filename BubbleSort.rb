86c66b3733428b44a0ae6e44705b376862b201a4
bencolclough <ben@tourdust.com>
2020-05-01 18:27:36 +0100

    tidy up comments

--- BubbleSort.rb
+++ BubbleSort.rb
@@ -1,12 +1,14 @@
 # method takes an array and returns a sorted array using the bubble sort methodology
+# the maximum number of iterations needed to sort an an array by bubble method is n-1 where n is the length of the array
+# After each i pass, the last i items in the array are already sorted and therefore are ignored in the bubble sort algorithm
 
 def bubble_sort (unsorted_array)
     iterations_count = 0
-    while iterations_count < unsorted_array.length # runs maximum times = length of array
-        i = 0 
+    while iterations_count < unsorted_array.length
+        i = 0
         changes = false
         unsorted_array.each do
-            break if i > (unsorted_array.length - 2 - iterations_count) # only iterates through unsorted elements in array
+            break if i > (unsorted_array.length - 2 - iterations_count)
             if unsorted_array[i] > unsorted_array[i+1] 
                 unsorted_array[i], unsorted_array[i+1] = unsorted_array[i+1], unsorted_array[i]
                 changes = true
@@ -15,13 +17,10 @@ def bubble_sort (unsorted_array)
                 i+=1
             end
         end
-    break if changes == false # stops running in all elements are sorted early
+    break if changes == false
     iterations_count += 1
-    
-    
-        
     end
-    puts "Done, I sorted your array in #{iterations_count} iterations, the sorted array is #{unsorted_array}"
+    puts "Done! I sorted your array in #{iterations_count} iterations, the sorted array is #{unsorted_array}"
 end
 
 puts bubble_sort([19,94,94,90,77,10,66,53,42,20,43,14,11,78,35,59,27,6])

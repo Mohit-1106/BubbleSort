1ad1f76333f5eb47ef5987d6144859d8641a3ad7
bencolclough <ben@tourdust.com>
2020-05-01 18:22:03 +0100

    introduce earilier stops to method when sorted

--- BubbleSort.rb
+++ BubbleSort.rb
@@ -1,23 +1,27 @@
 # method takes an array and returns a sorted array using the bubble sort methodology
 
 def bubble_sort (unsorted_array)
-    no_changes = 0 
-    while no_changes < unsorted_array.length-1 # keeps running until scores a complete round of no changes :)
-        i = 0
-        unsorted_array.each {
-            if i < unsorted_array.length - 1 #don't try to compare last item to non existant next item
-                if unsorted_array[i] > unsorted_array[i+1] 
+    iterations_count = 0
+    while iterations_count < unsorted_array.length # runs maximum times = length of array
+        i = 0 
+        changes = false
+        unsorted_array.each do
+            break if i > (unsorted_array.length - 2 - iterations_count) # only iterates through unsorted elements in array
+            if unsorted_array[i] > unsorted_array[i+1] 
                 unsorted_array[i], unsorted_array[i+1] = unsorted_array[i+1], unsorted_array[i]
-                nochanges = 0
-                end
+                changes = true
                 i+=1
-                no_changes += 1
+            else
+                i+=1
             end
-        }
-    end
-    p unsorted_array    
+        end
+    break if changes == false # stops running in all elements are sorted early
+    iterations_count += 1
     
-
+    
+        
+    end
+    puts "Done, I sorted your array in #{iterations_count} iterations, the sorted array is #{unsorted_array}"
 end
 
-puts bubble_sort([5,1,2])
+puts bubble_sort([19,94,94,90,77,10,66,53,42,20,43,14,11,78,35,59,27,6])
